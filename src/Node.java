da79520123bf848d7bfab174d461112e6607e654
Deniz1923 <ozgurdeniz2018@gmail.com>
2025-06-04 19:54:03 +0300

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -3,6 +3,8 @@ public class Node<T> {
     Node<T> next;
     Node<T> previous;
 
+    // Constructor of the node, referance nodes set to null for safety
+    
     public Node(T data){
         this.data = data;
         this.next = null;
