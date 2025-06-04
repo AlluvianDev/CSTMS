829a60b93efad49d99b0502ea4ebb0dd6f8311fd
Deniz1923 <ozgurdeniz2018@gmail.com>
2025-06-04 19:17:12 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "examplecommands.csv"; // Relative path as required
+        String filePath = "examplecommands.csv"; // Relative path of the input file
         List<Command> commands = FileIO.readCommands(filePath);
         CommandProcessor processor = new CommandProcessor();
         processor.processCommands(commands);
