3c4e5b54c7283958ab0d6505f4ec6d18bd5893e8
Deniz1923 <ozgurdeniz2018@gmail.com>
2025-06-08 10:58:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 
+// Entry point of the program
 public class Main {
     public static void main(String[] args) {
         try {
+            // Creates the main logic object
             CommandProcessor processor = new CommandProcessor();
             processor.processCommands();
+            // Give error if can not read the file
         } catch (IOException e) {
             System.err.println("Error reading commands file: " + e.getMessage());
             e.printStackTrace();
